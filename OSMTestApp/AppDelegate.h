6042eb78c40c8a5317fda56c7e4759374a919080
Steve Gifford <sjg@mousebirdconsulting.com>
2013-05-22 22:21:25 -0500

    Initial Commit

---
+++ OSMTestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OSMTestApp
+//
+//  Created by Steve Gifford on 5/22/13.
+//  Copyright (c) 2013 mousebird consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OSMTestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OSMTestApp
+//
+//  Created by Steve Gifford on 5/22/13.
+//  Copyright (c) 2013 mousebird consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
